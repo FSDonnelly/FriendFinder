0d28c53f8e9f9ce1307165774d4a6bb7d86eba71
FSDonnelly <donnelly_frank@yahoo.com>
2018-12-17 18:18:31 -0800

    finished most of everything, having issue deploying with heroku

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Thanos',
+		photo: 'https://vignette.wikia.nocookie.net/marveldatabase/images/a/a7/Thanos_%28Earth-12041%29_from_Marvel%27s_Avengers_Assemble_Season_2_12_001.png/revision/latest?cb=20160408233716',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Iron Man',
+		photo: 'https://vignette.wikia.nocookie.net/marveldatabase/images/7/74/Iron_Man_MK_42_Armor_-_Marvel_Avengers_Alliance_%282%29.png/revision/latest?cb=20160505084907',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Captain America',
+		photo: 'https://cdn.vox-cdn.com/thumbor/Z4yyRrPenLpg0ExhHpetrveJiuY=/0x0:2034x1356/1200x800/filters:focal(0x0:2034x1356)/cdn.vox-cdn.com/uploads/chorus_image/image/46293074/Avengers254d1158022f16-2040.0.0.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Thor',
+		photo: 'https://vignette.wikia.nocookie.net/avengersalliance/images/1/1d/Avengers_Thor_Right_Portrait_Art.png/revision/latest?cb=20130629135340',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Black Widow',
+		photo: 'https://static.comicvine.com/uploads/original/9/93477/2544794-the_avengers_black_widow_1.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Hulk',
+		photo: 'https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/06/15/Pictures/_1caf1900-7098-11e8-bbf6-b72314b60444.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Star-Lord',
+		photo: 'https://assets2.ignimgs.com/2014/11/12/guardians-chrispratt-1280ajpg-bc1754_400w.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Gamora',
+		photo: 'https://img.cinemablend.com/cb/0/8/8/5/3/6/088536673e7d92d183fcc584f26c5603577ec5886c9f8978e7e49ac9f131bcf5.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ant Man',
+		photo: 'https://starfriday.files.wordpress.com/2015/06/paul-rudd-as-antman.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Ego',
+		photo: 'http://cdn.epicstream.com/assets/uploads/featurescover/760x400/EgoCover.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
